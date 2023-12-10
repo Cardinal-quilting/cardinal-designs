4b86de3d9b50bac502e785112d65154173b86780
Andrew Davis <davisad@alum.mit.edu>
2023-12-10 12:51:29 -0500

    fixed slight bug in background image

--- crdnl/src/js/project-page/main-displays/project/background-image.js
+++ crdnl/src/js/project-page/main-displays/project/background-image.js
@@ -93,6 +93,11 @@ class BackgroundImage extends Component {
         const [height, width] = this.props.project_dimensions();
         const height_px = String(height) + "px", width_px = String(width) + "px";
 
+        // If the aspect ratio is <1.0, translate the image so that it is centered when the background_image_translation parameter is (0, 0).
+        // This means that the default behavior ((0, 0) tranlation and 1.0 zoom) means the background image matches the height exactly and is centered.
+        // Altnatively, we could set the height of the image to min(height, width) so that the default is that it fits exactly in the project.
+        const image_translation = -50*Math.max(0.0, 1.0-this.props.project_settings.aspect_ratio);
+
         return (
             <div
                 onMouseEnter={this.mouse_enter}
@@ -106,6 +111,7 @@ class BackgroundImage extends Component {
             >
                 <img src={logo} className="logo" alt="logo"
                 style={{
+                    transform: `translate(${image_translation}%, 0%)`,
                     opacity: this.props.project_settings.background_image_opacity,
                     height: height_px
             }}/>
